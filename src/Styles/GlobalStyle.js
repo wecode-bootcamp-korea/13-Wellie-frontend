da2b30942ef61c46c09dbb5737906485f4cababf
choihaneul9545 <trooioi9545@gmail.com>
2020-11-04 14:29:56 +0900

    Add: styled components 기본 세팅

---
+++ src/Styles/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap');
+
+  ${reset}
+
+  body{
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', 'Noto Serif KR', 'sans-serif', 'serif';
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  button,
+  input {
+    outline: none;
+    border: none;
+  }
+`;
+
+export default GlobalStyle;
